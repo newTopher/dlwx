61c413121fe7fc0ab93118e312646f83cc6d4a3c
zhutaofen <575956381@qq.com>
2014-02-22 01:21:05 +0800

    css js fix

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,7 +6,6 @@
 
     <!-- blueprint CSS framework -->
     <script src="<?php echo Yii::app()->getBaseUrl(); ?>/js/jquery-1.7.2.min.js"></script>
-    <script src="<?php echo Yii::app()->getBaseUrl(); ?>/js/jquery.validator.js"></script>
     <link id="bs-css" href="<?php echo Yii::app()->getBaseUrl(); ?>/css/bootstrap-cerulean.css" rel="stylesheet">
     <style type="text/css">
         body {
@@ -16,23 +15,9 @@
             padding: 9px 0;
         }
     </style>
-    <link href="<?php echo Yii::app()->getBaseUrl(); ?>/css/bootstrap-responsive.css" rel="stylesheet">
+
     <link href="<?php echo Yii::app()->getBaseUrl(); ?>/css/charisma-app.css" rel="stylesheet">
-    <link href="<?php echo Yii::app()->getBaseUrl(); ?>/css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/fullcalendar.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/fullcalendar.print.css' rel='stylesheet'  media='print'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/chosen.css' rel='stylesheet'>
     <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/uniform.default.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/colorbox.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/jquery.cleditor.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/jquery.noty.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/noty_theme_default.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/elfinder.min.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/elfinder.theme.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/jquery.iphone.toggle.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/opa-icons.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/uploadify.css' rel='stylesheet'>
-    <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/jquery.validator.css' rel='stylesheet'>
     <link href='<?php echo Yii::app()->getBaseUrl(); ?>/css/notifIt.css' rel='stylesheet'>
 </head>
 
