651bfbdc0edff7d8c772d009a17b360b8e867c25
zhuxiongpeng <keller927@gmail.com>
2014-03-03 10:16:22 +0800

    login-background

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -9,9 +9,7 @@
     <link id="bs-css" href="<?php echo Yii::app()->getBaseUrl(); ?>/css/bootstrap-cerulean.css" rel="stylesheet">
     <script src="<?php echo Yii::app()->request->baseUrl; ?>/css/uploadify.css"></script>
     <style type="text/css">
-        body {
-            padding-bottom: 40px;
-        }
+
         .sidebar-nav {
             padding: 9px 0;
         }
